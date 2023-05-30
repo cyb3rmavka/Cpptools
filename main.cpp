958f4a93132387ae474f69b9d947902c99d83cd1
cyb3rmavka <pavlenko.maksym.a@gmail.com>
2023-05-31 01:24:26 +0300

    add print map, pair, unordered map

--- main.cpp
+++ main.cpp
@@ -9,8 +9,36 @@
 
 int main(){
     // Example usage
+
     std::vector<std::vector<int>> vec = {{1,2}, {3, 4}};
     tools::print(vec);
+    std::pair<int,int> p = std::make_pair(1,2);
+    tools::print(p);
+
+    std::map<int, std::string> testData;
+
+    testData.insert(std::make_pair(1, "John"));
+    testData.insert(std::make_pair(2, "Alice"));
+    testData.insert(std::make_pair(3, "Bob"));
+    testData.insert(std::make_pair(4, "Sarah"));
+
+    std::map<int, double> floatData;
+
+    floatData.insert(std::make_pair(1, 3.14));
+    floatData.insert(std::make_pair(2, 2.718));
+    floatData.insert(std::make_pair(3, 1.618));
+    floatData.insert(std::make_pair(4, 0.577));
+
+    std::map<int, std::vector<int>> vectorData;
+
+    vectorData.insert(std::make_pair(1, std::vector<int>{1, 2, 3}));
+    vectorData.insert(std::make_pair(2, std::vector<int>{4, 5}));
+    vectorData.insert(std::make_pair(3, std::vector<int>{6}));
+    vectorData.insert(std::make_pair(4, std::vector<int>{7, 8, 9, 10}));
+
+    tools::print(testData);
+    tools::print(floatData);
+    tools::print(vectorData);
 
 
     return 0;
--- print.h
+++ print.h
@@ -4,7 +4,7 @@
 
 #include <vector>
 #include <iostream>
-
+#include <unordered_map>
 namespace tools {
 
     template<typename T>
@@ -21,19 +21,45 @@ namespace tools {
     }
 
     template <typename T>
-    void print(const std::list<T>& vec){
-        for (const auto& element : vec){
+    void print(const std::list<T>& l){
+        for (const auto& element : l){
             print(element);
         }
         std::cout << std::endl;
     }
 
     template <typename T>
-    void print(const std::set<T>& vec){
-        for (const auto& element : vec){
+    void print(const std::set<T>& s){
+        for (const auto& element : s){
             print(element);
         }
         std::cout << std::endl;
     }
+
+    template <typename T1, typename T2>
+    void print(const std::pair<T1, T2>& p){
+        print(p.first);
+        print(p.second);
+        std::cout << std::endl;
+    }
+
+    template <typename T1, typename T2>
+    void print(const std::map<T1, T2>& m){
+        for (const auto& element : m) {
+            print(element.first);
+            print(element.second);
+        }
+        std::cout << std::endl;
+    }
+
+    template <typename T1, typename T2>
+    void print(const std::unordered_map<T1, T2>& m){
+        for (const auto& element : m) {
+            print(element.first);
+            print(element.second);
+        }
+        std::cout << std::endl;
+    }
+
 }
 #endif
