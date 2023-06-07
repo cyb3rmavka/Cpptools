d998cf31a14e9910e89e02e3f6f98f0fd589b848
cyb3rmavka <pavlenko.maksym.a@gmail.com>
2023-06-08 01:42:12 +0300

    add function for split string

--- string_tools.h
+++ string_tools.h
@@ -1,4 +1,24 @@
 #ifndef CPPTOOLS_STRING_TOOLS_H
 #define CPPTOOLS_STRING_TOOLS_H
 
+#include <vector>
+#include <string>
+#include <sstream>
+
+namespace tools {
+
+    std::vector<std::string> splitString(const std::string& input, char delimiter) {
+        std::vector<std::string> substrings;
+        std::stringstream ss(input);
+        std::string substring;
+
+        while (std::getline(ss, substring, delimiter)) {
+            substrings.emplace_back(std::move(substring));
+        }
+
+        return substrings;
+    }
+
+}
+
 #endif
