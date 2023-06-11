2fe934c839fcf707be108b5ec901bd5b8dbbcf8c
cyb3rmavka <pavlenko.maksym.a@gmail.com>
2023-06-11 20:34:31 +0300

    add new func

--- tools_file_system.h
+++ tools_file_system.h
@@ -1,4 +1,17 @@
 #ifndef CPPTOOLS_TOOLS_FILE_SYSTEM_H
 #define CPPTOOLS_TOOLS_FILE_SYSTEM_H
 
+#include <filesystem>
+
+namespace tools{
+
+    namespace filesystem{
+        bool isPathValid(const std::string& path) {
+            std::filesystem::path filePath(path);
+            return filePath.has_filename();
+        }
+    }
+
+}
+
 #endif
