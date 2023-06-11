0fb6b739516ed3bd99269513351c2ed09fddfe54
cyb3rmavka <pavlenko.maksym.a@gmail.com>
2023-06-11 20:36:13 +0300

    add new func for formatted path

--- tools_file_system.h
+++ tools_file_system.h
@@ -2,13 +2,28 @@
 #define CPPTOOLS_TOOLS_FILE_SYSTEM_H
 
 #include <filesystem>
+#include <algorithm>
 
 namespace tools{
 
-    namespace filesystem{
-        bool isPathValid(const std::string& path) {
+    namespace filesystem {
+        bool isPathValid(const std::string &path) {
             std::filesystem::path filePath(path);
             return filePath.has_filename();
+        }
+
+        std::string formatPath(const std::string &path) {
+            std::string formattedPath = path;
+
+#ifdef _WIN32
+            // Windows path formatting
+            std::replace(formattedPath.begin(), formattedPath.end(), '/', '\\');
+#else
+            // Linux and Mac path formatting
+            std::replace(formattedPath.begin(), formattedPath.end(), '\\', '/');
+#endif
+
+            return formattedPath;
         }
     }
 
