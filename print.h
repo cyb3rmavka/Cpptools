3709c09b9b197fefdc55179975bfc73f0654d9cd
cyb3rmavka <pavlenko.maksym.a@gmail.com>
2023-06-14 00:02:57 +0300

    fix namespace print

--- print.h
+++ print.h
@@ -4,61 +4,62 @@
 #include <vector>
 #include <iostream>
 #include <unordered_map>
+
 namespace tools {
-
-    template<typename T>
-    void print(T el){
-        std::cout << el << " ";
-    }
-
-    template <typename T>
-    void print(const std::vector<T>& vec){
-        for (const auto& element : vec){
-            print(element);
+    namespace {
+        template<typename T>
+        void print(T el) {
+            std::cout << el << " ";
         }
-        std::cout << std::endl;
-    }
 
-    template <typename T>
-    void print(const std::list<T>& l){
-        for (const auto& element : l){
-            print(element);
+        template<typename T>
+        void print(const std::vector<T> &vec) {
+            for (const auto &element: vec) {
+                print(element);
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
-    }
 
-    template <typename T>
-    void print(const std::set<T>& s){
-        for (const auto& element : s){
-            print(element);
+        template<typename T>
+        void print(const std::list<T> &l) {
+            for (const auto &element: l) {
+                print(element);
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
-    }
 
-    template <typename T1, typename T2>
-    void print(const std::pair<T1, T2>& p){
-        print(p.first);
-        print(p.second);
-        std::cout << std::endl;
-    }
-
-    template <typename T1, typename T2>
-    void print(const std::map<T1, T2>& m){
-        for (const auto& element : m) {
-            print(element.first);
-            print(element.second);
+        template<typename T>
+        void print(const std::set<T> &s) {
+            for (const auto &element: s) {
+                print(element);
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
-    }
 
-    template <typename T1, typename T2>
-    void print(const std::unordered_map<T1, T2>& m){
-        for (const auto& element : m) {
-            print(element.first);
-            print(element.second);
+        template<typename T1, typename T2>
+        void print(const std::pair<T1, T2> &p) {
+            print(p.first);
+            print(p.second);
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
-    }
 
+        template<typename T1, typename T2>
+        void print(const std::map<T1, T2> &m) {
+            for (const auto &element: m) {
+                print(element.first);
+                print(element.second);
+            }
+            std::cout << std::endl;
+        }
+
+        template<typename T1, typename T2>
+        void print(const std::unordered_map<T1, T2> &m) {
+            for (const auto &element: m) {
+                print(element.first);
+                print(element.second);
+            }
+            std::cout << std::endl;
+        }
+    }
 }
 #endif
