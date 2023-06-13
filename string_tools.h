3b048dcc930fd0ad653046d377d11dede0d2f2a7
cyb3rmavka <pavlenko.maksym.a@gmail.com>
2023-06-14 00:02:15 +0300

    fix namespace string

--- string_tools.h
+++ string_tools.h
@@ -7,49 +7,48 @@
 #include <unordered_set>
 
 namespace tools {
+    namespace string {
+        std::vector<std::string> splitString(const std::string &input, char delimiter) {
+            std::vector<std::string> substrings;
+            std::stringstream ss(input);
+            std::string substring;
 
-    std::vector<std::string> splitString(const std::string& input, char delimiter) {
-        std::vector<std::string> substrings;
-        std::stringstream ss(input);
-        std::string substring;
-
-        while (std::getline(ss, substring, delimiter)) {
-            substrings.emplace_back(std::move(substring));
-        }
-
-        return substrings;
-    }
-
-    std::string combineStrings(const std::vector<std::string>& strings) {
-        std::string combinedString;
-        combinedString.reserve(256);
-
-        for (const auto& str : strings) {
-            combinedString += str;
-        }
-
-        return combinedString;
-    }
-
-    std::string cutWords(const std::string& input, const std::unordered_set<std::string>& wordsToCut) {
-        std::stringstream ss(input);
-        std::string word;
-        std::string result;
-
-        while (ss >> word) {
-            if (wordsToCut.find(word) == wordsToCut.end()) {
-                result += std::move(word) + " ";
+            while (std::getline(ss, substring, delimiter)) {
+                substrings.emplace_back(std::move(substring));
             }
+
+            return substrings;
         }
 
-        if (!result.empty()) {
-            result.pop_back();  // Remove the trailing space
+        std::string combineStrings(const std::vector<std::string> &strings) {
+            std::string combinedString;
+            combinedString.reserve(256);
+
+            for (const auto &str: strings) {
+                combinedString += str;
+            }
+
+            return combinedString;
         }
 
-        return result;
+        std::string cutWords(const std::string &input, const std::unordered_set<std::string> &wordsToCut) {
+            std::stringstream ss(input);
+            std::string word;
+            std::string result;
+
+            while (ss >> word) {
+                if (wordsToCut.find(word) == wordsToCut.end()) {
+                    result += std::move(word) + " ";
+                }
+            }
+
+            if (!result.empty()) {
+                result.pop_back();  // Remove the trailing space
+            }
+
+            return result;
+        }
     }
-
-
 
 }
 
